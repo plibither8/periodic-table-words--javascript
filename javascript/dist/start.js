41ffe63d0fe469a374ac33d10eede6dd99f8bb59
Mihir Chaturvedi <plibither8@gmail.com>
2018-03-05 00:16:50 +0530

    Output using `console.table` module

--- javascript/dist/start.js
+++ javascript/dist/start.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var readline = require('readline');
+var table = require('console.table');
 var symbols = require('./symbols');
 var check = require('./check');
 var rl = readline.createInterface({
@@ -16,14 +17,17 @@ function startFunction() {
             var result = main(sanitisedInput);
             var wordFormed = result[1];
             if (result[0]) {
+                var outputTable = [];
                 console.log("\n" + userInput + " is possible.\n");
                 for (var i = 0; i < wordFormed.length; i++) {
                     wordFormed[i] = wordFormed[i].charAt(0).toUpperCase() + wordFormed[i].slice(1);
                 }
                 console.log(wordFormed.join(" ") + "\n");
                 for (var i = 0; i < wordFormed.length; i++) {
-                    console.log(wordFormed[i] + ": " + symbolNameObject[wordFormed[i]]);
+                    var tempObject = { "Symbol": wordFormed[i], "Element": symbolNameObject[wordFormed[i]] };
+                    outputTable.push(tempObject);
                 }
+                console.table(outputTable);
             }
             else {
                 console.log("\n" + userInput + " is not possible.");
